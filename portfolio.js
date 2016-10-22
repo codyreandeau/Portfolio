050b8575082b9ff8727203390dd3408caf1dbb9b
Cody Reandeau <codyreandeau@yahoo.com>
2016-10-21 22:28:07 -0400

    Created javascript fil: portfolio.js. Wrote function to hide right nav bar when the menu-icon is clicked

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var nav = document.getElementById('nav-right');    if(nav.style.display == "inline-block") { 
+    nav.style.display = "none";
+  }
+  else { 
+    nav.style.display = "inline-block";
+  }
+}
